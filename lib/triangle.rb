fa2b3499eb4033c059ac474ce728e27786f0c813
geoffrey woodcock <theatomproject@gmail.com>
2018-05-09 20:09:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,34 @@
 class Triangle
  
-  attr_accessor :x, :y, :z
+#   attr_accessor :x, :y, :z
  
-  def initialize(x,y,z)
-    @x = x
-    @y = y
-    @z = z
-    @sides = [x,y,z]
-  end
+#   def initialize(x,y,z)
+#     @x = x
+#     @y = y
+#     @z = z
+#     @sides = [x,y,z]
+#   end
   
-  def kind
-    if (x == y) && (x == z) && (y == z)
-      :equilateral
-    elsif 
-      (x == y) || (x == z) || (y == z)
-      :isosceles
-    else 
-      :scalene
-    end
-    if !triangle_true
-      raise TriangleError
-    end
-  end
+#   def kind
+#     if (x == y) && (x == z) && (y == z)
+#       :equilateral
+#     elsif 
+#       (x == y) || (x == z) || (y == z)
+#       :isosceles
+#     else 
+#       :scalene
+#     end
+#     if !triangle_true
+#       raise TriangleError
+#     end
+#   end
       
   
-  def triangle_true
-   (x + y < z)|| (y + z < x) || (x + z < y) && (x > 0) && (y > 0) && (z > 0)
-  end
+#   def triangle_true
+#   (x + y < z)|| (y + z < x) || (x + z < y) && (x > 0) && (y > 0) && (z > 0)
+#   end
   
-  class TriangleError < StandardError
-  end 
+#   class TriangleError < StandardError
+#   end 
  
-end
+# end
