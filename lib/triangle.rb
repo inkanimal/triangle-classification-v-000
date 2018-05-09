f2f2e693624af5e936cd587d2b8e5dae946e36b7
geoffrey woodcock <theatomproject@gmail.com>
2018-05-09 19:22:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+ 
+  attr_accessor :side_x, :side_y, :side_z
+ 
+  def initialize(side_x, side_y, side_z)
+    @side_x = side_x
+    @side_y = side_y
+    @side_z = side_z
+  end
+  
+  def kind 
+    if triangle_true != true
+      raise TriangleError 
+    else 
+      triangle_true
+    end 
+    if side_x == side_y && side_x == side_z && side_y == side_z
+      :equilateral
+    elsif 
+      side_x == side_y || side_x == side_z || side_y == side_z
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+      
+  
+  def triangle_true
+   side_x + side_y < side_z || side_y + side_z < side_x || side_x + side_z < side_y && side_x > 0 && side_y > 0 && side_z > 0
+  end
+  
+  class TriangleError < StandardError
+  end 
+ 
 end
