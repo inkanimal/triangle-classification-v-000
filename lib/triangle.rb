9dfe63b95dc702e5ccc71de43b96c6a49392bc91
geoffrey woodcock <theatomproject@gmail.com>
2018-05-09 19:30:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,7 +26,7 @@ class Triangle
       
   
   def triangle_true
-   side_x + side_y < side_z || side_y + side_z < side_x || side_x + side_z < side_y && side_x > 0 && side_y > 0 && side_z > 0
+   (side_x + side_y < side_z)|| (side_y + side_z < side_x) || (side_x + side_z < side_y) && (side_x > 0) && (side_y > 0) && (side_z > 0)
   end
   
   class TriangleError < StandardError
