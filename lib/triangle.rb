be14cb9e6da1ac06e0f20c110850a51bfd8d7c3a
geoffrey woodcock <theatomproject@gmail.com>
2018-05-09 20:14:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
 
   def kind
-    if triangle_true == true #&& kosher_triangle2 == true 
+    if triangle_true == true 
       puts true 
     else 
       raise TriangleError 
@@ -26,42 +26,9 @@ class Triangle
   def triangle_true 
    ((x > 0) && (y > 0) && (z > 0)) && ((x + y > z) && (x + z > y) && (y + z > x))
   end 
-  
-  # def kosher_triangle2 
-  #   x + y > z && x + z > y && y + z > x
-  # end 
 end
 
 class TriangleError < StandardError
   
 end
-#   def initialize(x,y,z)
-#     @x = x
-#     @y = y
-#     @z = z
-#     @sides = [x,y,z]
-#   end
-  
-#   def kind
-#     if (x == y) && (x == z) && (y == z)
-#       :equilateral
-#     elsif 
-#       (x == y) || (x == z) || (y == z)
-#       :isosceles
-#     else 
-#       :scalene
-#     end
-#     if !triangle_true
-#       raise TriangleError
-#     end
-#   end
-      
-  
-#   def triangle_true
-#   (x + y < z)|| (y + z < x) || (x + z < y) && (x > 0) && (y > 0) && (z > 0)
-#   end
-  
-#   class TriangleError < StandardError
-#   end 
- 
-# end
+
